9b202aab8ad7df7da575b97f1d1a76664213ffa0
Kiri <kirilynne@gmail.com>
2021-11-22 21:25:48 -0500

    add content to Footer.js

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,16 +1,25 @@
-<footer>
-    <div class="contact-content">
-        <nav class="nav-wrapper nav-deets">
-            <a href="#header" class="my-name">Kiri Smith</a>
-            <ul class="right">
-                <li><a href="https://docs.google.com/document/d/1ZheW1hS7MNjaeBhSnr3-krOhUXSlYMewwBU9NKeDZeA/edit?usp=sharing"
-                    target="_blank">585.739.2378</a></li>
-                <li><a href="https://docs.google.com/document/d/1ZheW1hS7MNjaeBhSnr3-krOhUXSlYMewwBU9NKeDZeA/edit?usp=sharing"
-                    target="_blank">kirilynne@gmail.com</a></li>
-                <li><a href="https://www.linkedin.com/in/kiri-lynne-smith/" target="_blank">Let's
-                    Connect!</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-</footer>
+import React from 'react';
+import '../styles/Header.css';
+
+function Footer() {
+    return (
+        <footer>
+            <div class="contact-content">
+                <nav class="nav-wrapper nav-deets">
+                    <a href="#header" class="my-name">Kiri Smith</a>
+                    <ul class="right">
+                        <li><a href="https://docs.google.com/document/d/1ZheW1hS7MNjaeBhSnr3-krOhUXSlYMewwBU9NKeDZeA/edit?usp=sharing"
+                            target="_blank">585.739.2378</a></li>
+                        <li><a href="https://docs.google.com/document/d/1ZheW1hS7MNjaeBhSnr3-krOhUXSlYMewwBU9NKeDZeA/edit?usp=sharing"
+                            target="_blank">kirilynne@gmail.com</a></li>
+                        <li><a href="https://www.linkedin.com/in/kiri-lynne-smith/" target="_blank">Let's
+                            Connect!</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
